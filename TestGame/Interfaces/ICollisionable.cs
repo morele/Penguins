59cf73595dd1d9d7e2e514e4e82fc4fe77cabf9a
Mateusz Łysień <matelys957@student.polsl.pl>
2015-11-11 10:50:25 +0100

    Grawitacja
    Dodałem grawitację do gry. Pingwiny mają swoją masę, przez co reagują na
    "pole grawitacyjne"

---
+++ TestGame/Interfaces/ICollisionable.cs
@@ -0,0 +1,8 @@
+﻿namespace TestGame.Interfaces
+{
+    public interface ICollisionable
+    {
+        bool IsCollisionDetect(GameObject collisionObject);
+        void OnCollisionDetect(GameObject collisionObject);
+    }
+}
