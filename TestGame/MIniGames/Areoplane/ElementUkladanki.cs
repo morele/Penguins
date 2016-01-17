621d71224c3084c913441806c3982d7d46769c7a
Łukasz Gawin <gawinster@gmail.com>
2016-01-17 16:36:26 +0100

    Minigra Samoloty

---
+++ TestGame/MIniGames/Areoplane/ElementUkladanki.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TestGame.MIniGames.Areoplane
+{
+
+    class ElementUkladanki
+    {
+        private GraphicsDevice _graphicsDevice;
+        private SpriteBatch _spriteBatch;
+        private ContentManager _content;
+        private Texture2D Grafika;
+        private Rectangle AktualnePolozenie;
+        private Vector2 PolozenieWPrzyborniku;
+        private Rectangle PolozenieNaUkladance;
+        private bool Zlapany;
+
+        public bool NaMejscu
+        {
+            get;
+            private set;
+        }
+        private float czas;
+
+        public ElementUkladanki(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch, ContentManager content, string nazwa_sprita, Vector2 PolUklad)
+        {
+            _graphicsDevice = graphicsDevice;
+            _spriteBatch = spriteBatch;
+            _content = content;
+
+            Grafika = content.Load<Texture2D>("Minigry/Areoplane/" + nazwa_sprita);
+            PolozenieNaUkladance = new Rectangle((int)PolUklad.X, (int)PolUklad.Y, 2, 2);
+            Zlapany = false;
+            NaMejscu = false;
+        }
+        public int ZaladujDoPrzybornika(int y)
+        {
+            PolozenieWPrzyborniku.X = _graphicsDevice.Viewport.Width - 200 - Grafika.Width / 2 - 10;
+            PolozenieWPrzyborniku.Y = y;
+            AktualnePolozenie = new Rectangle((int)PolozenieWPrzyborniku.X, (int)PolozenieWPrzyborniku.Y, Grafika.Width, Grafika.Height);
+            return y + Grafika.Height + 5;
+        }
+
+        public bool Zlap(int x, int y)
+        {
+            if (!NaMejscu)
+            {
+                Rectangle Mysz = new Rectangle(x, y, 1, 1);
+                if (Mysz.Intersects(AktualnePolozenie))
+                {
+                    Zlapany = true;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Scrolluj(int y)
+        {
+            if (!NaMejscu)
+            {
+                if (AktualnePolozenie.Y == PolozenieWPrzyborniku.Y)
+                    AktualnePolozenie.Y += y;
+
+                PolozenieWPrzyborniku.Y += y;
+
+            }
+        }
+
+        public void Przesun(int x, int y)
+        {
+            if (Zlapany && !NaMejscu)
+            {
+                AktualnePolozenie.X = x - Grafika.Width / 2;
+                AktualnePolozenie.Y = y - Grafika.Height / 2;
+            }
+        }
+
+        public void Powrot(GameTime gt)
+        {
+            if (!Zlapany && !NaMejscu)
+            {
+                czas += (float)gt.ElapsedGameTime.TotalMilliseconds;
+
+                if (czas >= 16f)
+                {
+                    if (AktualnePolozenie.X != PolozenieWPrzyborniku.X || AktualnePolozenie.Y != PolozenieWPrzyborniku.Y)
+                    {
+                        Vector2 aktualne = new Vector2(AktualnePolozenie.X, AktualnePolozenie.Y);
+                        float distance = Vector2.Distance(aktualne, PolozenieWPrzyborniku);
+
+                        AktualnePolozenie.X += (int)(((float)(PolozenieWPrzyborniku.X - AktualnePolozenie.X)) / (distance / 2));
+                        AktualnePolozenie.Y += (int)(((float)(PolozenieWPrzyborniku.Y - AktualnePolozenie.Y)) / (distance / 2));
+                    }
+
+                    czas = 0;
+                }
+            }
+        }
+
+        public void Odloz()
+        {
+            Rectangle RogElementu = new Rectangle(AktualnePolozenie.X - 5, AktualnePolozenie.Y - 5, 10, 10);
+
+            if (PolozenieNaUkladance.Intersects(RogElementu))
+            {
+                AktualnePolozenie.X = PolozenieNaUkladance.X;
+                AktualnePolozenie.Y = PolozenieNaUkladance.Y;
+                NaMejscu = true;
+            }
+
+            if (Zlapany)
+                Zlapany = false;
+        }
+
+        public void Wyswiwietl()
+        {
+            _spriteBatch.Draw(Grafika, AktualnePolozenie, Color.White);
+
+        }
+    }
+}
---
+++ TestGame/MIniGames/Areoplane/SkladanieSamolotu.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TestGame.MIniGames.Areoplane
+{
+    class SkladanieSamolotu
+    {
+        private GraphicsDevice _graphicsDevice;
+        private SpriteBatch _spriteBatch;
+        private ContentManager _content;
+
+        ElementUkladanki[] elementy;
+        private Texture2D Tlo;
+        int zlapany;
+        int Mysz;
+
+        public bool EndOfGame
+        {
+            get;
+            private set;
+        }
+
+        public SkladanieSamolotu(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch, ContentManager content)
+        {
+            _graphicsDevice = graphicsDevice;
+            _spriteBatch = spriteBatch;
+            _content = content;
+
+
+            Tlo = _content.Load<Texture2D>("Minigry/Areoplane/SamolotTlo");
+            zlapany = -1;
+            int y = 5;
+
+            elementy = new ElementUkladanki[14];
+            elementy[0] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "0", new Vector2(93f, 118f));
+            elementy[1] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "1", new Vector2(154f, 317f));
+            elementy[2] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "2", new Vector2(138f, 312f));
+            elementy[3] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "3", new Vector2(220f, 381f));
+            elementy[4] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "4", new Vector2(201f, 304f));
+            elementy[5] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "5", new Vector2(507f, 233f));
+            elementy[6] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "6", new Vector2(434f, 102f));
+            elementy[7] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "7", new Vector2(117f, 184f));
+            elementy[8] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "8", new Vector2(276f, 277f));
+            elementy[9] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "9", new Vector2(91f, 83f));
+            elementy[10] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "10", new Vector2(337f, 265f));
+            elementy[11] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "11", new Vector2(102f, 206f));
+            elementy[12] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "12", new Vector2(94f, 164f));
+            elementy[13] = new ElementUkladanki(_graphicsDevice, spriteBatch, _content, "13", new Vector2(57f, 40f));
+
+            for (int i = 0; i < 14; i++)
+            {
+                y = elementy[i].ZaladujDoPrzybornika(y);
+            }
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                for (int i = 0; i < 14; i++)
+                    if (elementy[i].Zlap(Mouse.GetState().X, Mouse.GetState().Y) == true)
+                    {
+                        zlapany = i;
+                        break;
+                    }
+            }
+
+
+
+            if (Mysz != Mouse.GetState().ScrollWheelValue)
+            {
+
+                if (Mouse.GetState().ScrollWheelValue > -2300 && Mouse.GetState().ScrollWheelValue <= 0)
+                {
+                    int y = (int)((Mouse.GetState().ScrollWheelValue - Mysz) / 3);
+                    Mysz = Mouse.GetState().ScrollWheelValue;
+
+                    for (int i = 0; i < 14; i++)
+                    {
+                        elementy[i].Scrolluj(y);
+                    }
+                }
+            }
+
+
+
+            if (Mouse.GetState().LeftButton == ButtonState.Released)
+            {
+                if (zlapany > -1)
+                    elementy[zlapany].Odloz();
+                zlapany = -1;
+            }
+
+            if (zlapany > -1)
+            {
+                elementy[zlapany].Przesun(Mouse.GetState().X, Mouse.GetState().Y);
+            }
+
+            for (int i = 0; i < 14; i++)
+            {
+                elementy[i].Powrot(gameTime);
+            }
+            foreach (ElementUkladanki element in elementy)
+            {
+                EndOfGame = element.NaMejscu;
+            }
+        }
+
+        public void Draw()
+        {
+            _graphicsDevice.Clear(Color.Silver);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(Tlo, new Rectangle(0, 0, Tlo.Width, Tlo.Height), Color.White);
+
+            for (int i = 0; i < 14; i++)
+            {
+                elementy[i].Wyswiwietl();
+            }
+
+            _spriteBatch.End();
+
+        }
+
+    }
+}
